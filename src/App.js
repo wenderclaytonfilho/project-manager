d36a19f85117fdec434e6d04168d5080d63333f0
wenderclaytonfilho <rednew2727@gmail.com>
2024-02-15 09:49:31 -0300

    Alterando o JSX

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 function App() {
 
   const name = "Wender"
+  const url = "https://cdn-icons-png.flaticon.com/512/5969/5969113.png"
 
   function sum(a,b){
     return a+b
   }
-  
+
   return (
     <div className="App">
       <h1>Meu primeiro projeto React</h1>
-      <p>Soma, {sum(10,5)}</p>
+      <p>Me chamo, {name}</p>
+      <img src={url} alt="Minha Imagem"/>
     </div>
   );
 
